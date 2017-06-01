529e6d668c2f9832b57e866d3aa30cfa6be7e4b1
JeongEuiJin <hehar1020@gmail.com>
2017-06-02 00:28:56 +0900

    add inex detail html

--- django-app/polls/urls.py
+++ django-app/polls/urls.py
@@ -5,4 +5,8 @@ from polls import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<question_id>\d+)/$', views.detail),
+    url(r'^(?P<question_id>\d+)/results/$', views.results),
+    url(r'^(?P<question_id>\d+)/vote/$', views.vote),
+
 ]
--- django-app/polls/views.py
+++ django-app/polls/views.py
@@ -1,6 +1,29 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+
+from polls.models import Question
 
 
 def index(request):
-    return HttpResponse("hello, world .you're at the polls windows")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoseNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html',{'question':question})
+
+
+def results(request, question_id):
+    response = "your looking at the result of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("youre voting on question %s." % question_id)
